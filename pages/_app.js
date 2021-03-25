c0c59a852db4875f3c435de1137638f78ab727ac
Quan_Hoang_ <73420593+zziboyzz@users.noreply.github.com>
2021-03-25 21:49:20 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Footer from '../Component/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div style={{position:"absolute", top: 0, left: 0, width:"100%", backgroundColor:"red"}}>
+    <div style={{position:"absolute", top: 0, left: 0, width:"100%",}}>
       <App />
       
         <Component {...pageProps} />
